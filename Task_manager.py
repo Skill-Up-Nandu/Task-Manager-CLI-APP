713e4c3f4ee4e188e2e1bc86d3e2986898217e64
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-01 23:38:23 +0530

    function view task created

--- Task_manager.py
+++ Task_manager.py
@@ -13,21 +13,30 @@ def show_menu():
     print("5. Delete Task")
     print("6. Exit")
 
-# add new task
+# function to add new task
 def add_task():
     if not tasks:
         print("No Task Exists. Enter The First Task")
     title = input("\nWrite Your Task Here : ")
     tasks.append({'title' : title , 'done' : False})
 
-# while True:
-show_menu()
+# function to view all task
+def view_all_task():
+    print("\n=================== TASK LIST ====================\n")
+    if not tasks:
+        print("LIST IS EMPTY NOW !.")
+    for idx , task in enumerate(tasks , start = 1):
+        status = "✅" if task["done"] else "❌"
+        print(f"{idx}. {task['title']}  [{status}].")
+
+while True:
+    show_menu()
 
 # user selection to perform task
-choice = int(input("\nEnter Task Number : "))
-if choice == 1:
-    view_all_task()
-elif choice == 2:
-    add_task()
-else:
-    print("Invalid Task Number. TRY AGAIN!")
+    choice = int(input("\nEnter Task Number : "))
+    if choice == 1:
+        view_all_task()
+    elif choice == 2:
+        add_task()
+    else:
+        print("Invalid Task Number. TRY AGAIN!")
