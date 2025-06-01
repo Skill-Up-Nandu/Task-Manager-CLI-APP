a09d6d6caef3286a927595b9798699d55b2195de
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-01 14:35:14 +0530

    created task manager menu

---
+++ Task_manager.py
@@ -0,0 +1,17 @@
+# Task Manager "to-do-list" CLI-App
+
+# list to store all tasks in dictionery
+tasks = []
+
+# a function who show all menu
+def show_menu():
+    print("\n=================== TASK MANAGER ====================\n")
+    print("1. View All Tasks")
+    print("2. Add Task")
+    print("3. Mark As Done")
+    print("4. Modify Task")
+    print("5. Delete Task")
+    print("6. Exit")
+
+# while True:
+show_menu()
