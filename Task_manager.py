9b3abd9c34ad5a5ba78d9431f903d35645cf9367
Nandini Puranchand <behindthescenesgrowth@gmail.com>
2025-06-02 16:01:10 +0530

    add main function an call it

--- Task_manager.py
+++ Task_manager.py
@@ -5,6 +5,8 @@ tasks = []
 
 # a function who show all menu
 def show_menu():
+    """ Displays the main menu options for the Task Manager CLI application. """
+
     print("\n=================== TASK MANAGER ====================\n")
     print("1. View All Tasks")
     print("2. Add Task")
@@ -15,6 +17,10 @@ def show_menu():
 
 #  Helper Function to check weather list is empty or not
 def is_tasks_empty():
+    
+    """ Checks is the tasks list is empty.
+    Prints a message if empty and returns True, otherwise returns false. """
+
     if not tasks:
         print("\nList Is Empty Now.")
         return True
@@ -22,6 +28,10 @@ def is_tasks_empty():
 
 # function to add new task
 def add_task():
+
+    """Adds a new task to the tasks list.
+    Promots the user to enter a task title and appends it as a dictionary."""
+
     if not tasks:
         print("No Task Exists. Enter The First Task")
     title = input("\nWrite Your Task Here : ")
@@ -29,6 +39,10 @@ def add_task():
 
 # function to view all task
 def view_all_task():
+
+    """ Display all tasks in the tasks list with their status.
+    show a message if the list is empty. """
+
     print("\n=================== TASK LIST =====================\n")
     if not tasks:
         print("LIST IS EMPTY NOW !.")
@@ -38,6 +52,10 @@ def view_all_task():
 
 # function to mark task as done
 def mark_done():
+    
+    """ Marks a selected task as done.
+    Promotes the user for the task number and updates its status. """
+
     if is_tasks_empty():
         return
     view_all_task()
@@ -49,6 +67,9 @@ def mark_done():
 
 # function to modify the status
 def modify_status():
+     """ Modifies the status of a selected task.
+     Promotes the user for the task number and toggles its done status. """
+
     if is_tasks_empty():
         return
     view_all_task()
@@ -62,6 +83,10 @@ def modify_status():
 
 # function to delete task 
 def delete_task():
+
+    """ Delete the selected task form the tasks list.
+    promots the user for the task number and remove it form ths list. """
+
     if is_tasks_empty():
         return
     view_all_task()
@@ -75,28 +100,36 @@ def delete_task():
         print("\nInvalid Task Number.")
 
 
-# main loop 
-while True:
-    show_menu()
+def main():
+    
+    """ Main Loop for the Task Manager CLI APP. """
+
+    while True:
+        show_menu()
 
 # user selection to perform task
 # check input valid
-    try:
-        choice = int(input("\nEnter Task Number : "))
-    except ValueError:
-        print("\nInput Is Invalid. Try Again.")
-        continue
-    if choice == 1:
-        view_all_task()
-    elif choice == 2:
-        add_task()
-    elif choice == 3:
-        mark_done()
-    elif choice == 4:
-        modify_status()
-    elif choice == 5:
-        delete_task()
-    elif choice == 6:
-        break   # to exit from the loop
-    else:
-        print("Invalid Task Number. TRY AGAIN!")
+        try:
+            choice = int(input("\nEnter Task Number : "))
+        except ValueError:
+            print("\nInput Is Invalid. Try Again.")
+            continue
+        if choice == 1:
+            view_all_task()
+        elif choice == 2:
+            add_task()
+        elif choice == 3:
+            mark_done()
+        elif choice == 4:
+            modify_status()
+        elif choice == 5:
+            delete_task()
+        elif choice == 6:
+            break   # to exit from the loop
+        else:
+            print("Invalid Task Number. TRY AGAIN!")
+
+
+# very first execution
+if __name__ == "__main__":
+    main()
